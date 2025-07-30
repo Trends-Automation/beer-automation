bc43c9a247dc150fced435cb6f0273bc994ba780
luis-domingues <luisdomingues117@gmail.com>
2025-07-30 15:15:03 -0300

    git(server): reestruturar rotas de pagamento e otimizar middleware

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3001;
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
 
 const dispenserRouter = require('./src/routes/dispenser');
 app.use('/dispenser', dispenserRouter);
 
+/*
 const pagamentoRouter = require('./src/routes/pagamento');
 app.use('/pagamento', pagamentoRouter);
+*/
+
+const pagamentoPixRouter = require('./src/routes/pagamento_pix');
+app.use('/pagamento/pix', pagamentoPixRouter);
+
+const webhookRouter = require('./src/routes/webhook');
+app.use('/pagamento', webhookRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
