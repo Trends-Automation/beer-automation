1b793539f32d58bc1d00033451b95d158771d4e9
luis-domingues <luisdomingues117@gmail.com>
2025-08-04 14:15:14 -0300

    refactor(test): remover constante do tipo de chopp

--- backend/src/routes/pagamento.js
+++ backend/src/routes/pagamento.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/criar-link', async (req, res)=> {
-    const {tipo,ml,valor} = req.body;
+    const {ml,valor} = req.body;
 
-    if (!tipo || !ml || !valor) {
+    if (!ml || !valor) {
         return res.status(400).json({ error: 'Tipo, ml e valor são obrigatórios' });
     }
 
-    const link = `https://app.infinitepay.io/deeplink/payment?amount=${valor}&reason=Chopp+${ml}+${tipo}`;
+    const link = `https://app.infinitepay.io/deeplink/payment?amount=${valor}&reason=Chopp+${ml}`;
     res.json({link});
 });
 
