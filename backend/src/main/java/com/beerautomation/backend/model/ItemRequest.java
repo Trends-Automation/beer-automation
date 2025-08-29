7618b68c1cba5b8d49fc2fd85a140e1aee5985a9
luis-domingues <luisdomingues117@gmail.com>
2025-08-29 11:11:24 -0300

    feat(model): adicionar model ItemRequest

--- backend/src/main/java/com/beerautomation/backend/model/ItemRequest.java
+++ backend/src/main/java/com/beerautomation/backend/model/ItemRequest.java
@@ -1,4 +1,40 @@
 package com.beerautomation.backend.model;
 
 public class ItemRequest {
+    private String title;
+    private String description;
+    private int quantity;
+    private double unitPrice;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
